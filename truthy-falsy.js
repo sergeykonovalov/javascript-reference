ee4efb95ceb5c6229421edc86834dcda6e480f27
Sergey Konovalov <skonovalov@gmail.com>
2018-12-31 01:01:33 +0300

    Add info on truthy/falsy

---
+++ truthy-falsy.js
@@ -0,0 +1,12 @@
+const products = [];
+const product = products[0];
+
+// Truthy
+
+// Falsy
+
+if (product !== undefined) {
+  console.log('product found');
+} else {
+  console.log('product not found');
+}
